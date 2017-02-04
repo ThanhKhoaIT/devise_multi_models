18136a1113c6641f885cabe6c4576d6633603b39
KhoaRB <ThanhKhoaIT@gmail.com>
2017-02-04 22:35:20 +0700

    Init Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DeviseMultiModels::Application.config.secret_key_base = '61622a9dc93b615ff0a5fae56d8edf20321992cec4680174701fafc5c91eff114a019e5604167b595b1b8c02b709cde9cf564086c2cb068137987d04b393b27b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DeviseMultiModels::Application.config.session_store :cookie_store, key: '_devise_multi_models_session'
